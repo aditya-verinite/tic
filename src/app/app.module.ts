b5a3bd1d51d8ac14e4f7eff51637c96be5c3036b
Aditya Suryavanshi <AdityaS@VERINITE1.local>
2024-01-15 17:13:00 +0530

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameBoardComponent } from './game-board/game-board.component';
+
+import { SquareComponent } from './square/square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoardComponent,
+ 
+    SquareComponent
   ],
   imports: [
     BrowserModule,
